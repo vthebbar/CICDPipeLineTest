ca2ef1746dd93718391d92231628b184e4291dcd
Vishwanatha Thimmanna Hebbar <vishwa@Vishwanathas-MacBook-Pro.local>
2023-03-21 22:50:58 +0800

    first commit

---
+++ src/test/java/testCases/TestCaseOne.java
@@ -0,0 +1,19 @@
+package testCases;
+
+import org.testng.annotations.Test;
+
+public class TestCaseOne {
+
+	@Test
+	public void testOne() {
+		
+		System.out.println("Automation Project for CI CD-Test case one");
+	}
+	
+	
+	@Test
+	public void testTwo() {
+		
+		System.out.println("Automation Project for CI CD-Test case TWO");
+	}
+}
